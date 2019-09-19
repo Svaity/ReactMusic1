81ebf3476984fce9af30ec9d3fa2a853fac86bd9
Shrey <svaity@stevens.edu>
2019-09-18 21:43:14 -0400

    Create Tracks and User with basic schema

---
+++ app/users/schema.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+
+import graphene
+from graphene_django import DjangoObjectType
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
